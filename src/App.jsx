60d823f7c8cd07e6328577cdc77e4fda177db568
filipMaM <filip.mamcarczyk@gmail.com>
2024-02-05 20:13:49 +0100

    Lesson start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Main } from "./components/Main/Main";
+import { Modal } from "./components/Modal/Modal";
+
+function App() {
+    return (
+        <>
+            <h1>Witaj w lekcji o React Portal!</h1>
+            <Main />
+            <footer>zrozumiecReact.pl ©</footer>
+            <Modal />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,12 @@
+import { Modal } from "../Modal/Modal";
+import "./Section.css";
+
+export function Section() {
+    return (
+        <section>
+            <h2>Nie czekaj!</h2>
+            <h3>Otwórz modal!</h3>
+            <button>Otwórz</button>
+        </section>
+    );
+}
