16ea3f2870a9476eb9354daa2adb3d881035883f
Paweł Brzozowski <pawel@brzoza.net>
2024-08-21 23:09:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Wataha, sezon 1</h1>
+      <h2>Rok produkcji: 2015</h2>
+      <h2>Fabuła</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+        quidem debitis assumenda veniam ab nostrum vel, alias, cupiditate
+        doloremque id perspiciatis tempore, natus totam reprehenderit maxime
+        blanditiis aut quaerat eveniet.
+      </p>
+      <button>Pokaż spoiler</button>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, culpa?
+        Ex consectetur vero cupiditate! Corporis non, necessitatibus velit alias
+        similique dolor ad a veniam maiores, amet voluptas harum quo quia!
+      </p>
+    </>
+  );
 }
 
 export default App;
