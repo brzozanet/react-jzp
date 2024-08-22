41bf3976c90e504810da033dcc284b685e28a322
Paweł Brzozowski <pawel@brzoza.net>
2024-08-22 15:04:42 +0200

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 function App() {
-  const handleClick = () => {
-    alert("Button click!");
+  const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+  const [isWarningShown, setIsWarningShown] = useState(true);
+
+  const handleSpoilerShowClick = () => {
+    setIsSpoilerShown(true);
+    setIsWarningShown(false);
+  };
+
+  const handleShowWarningClick = () => {
+    setIsWarningShown(false);
   };
 
   return (
@@ -8,18 +18,28 @@ function App() {
       <h1>Wataha, sezon 1</h1>
       <h2>Rok produkcji: 2015</h2>
       <h2>Fabuła</h2>
+      {isWarningShown && (
+        <p>
+          Uwaga! Opis fabuły zawiera spoilery!{" "}
+          <button onClick={handleShowWarningClick}>X</button>
+        </p>
+      )}
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
         quidem debitis assumenda veniam ab nostrum vel, alias, cupiditate
-        doloremque id perspiciatis tempore, natus totam reprehenderit maxime
-        blanditiis aut quaerat eveniet.
+        doloremque id perspiciatis tempore.
       </p>
-      <button onClick={handleClick}>Pokaż spoiler</button>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, culpa?
-        Ex consectetur vero cupiditate! Corporis non, necessitatibus velit alias
-        similique dolor ad a veniam maiores, amet voluptas harum quo quia!
-      </p>
+      {isSpoilerShown ? (
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi iste,
+          laboriosam ipsa, saepe nemo sequi fugit at cumque, minima sint autem
+          numquam. Mollitia, laboriosam modi quae officiis, fugiat sit
+          temporibus quas provident nam blanditiis praesentium illo doloremque
+          sapiente corrupti repudiandae accusamus laborum.
+        </p>
+      ) : (
+        <button onClick={handleSpoilerShowClick}>Pokaż spoiler</button>
+      )}
     </>
   );
 }
