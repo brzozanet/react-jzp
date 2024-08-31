dab10b493cc60441bfaff4a936b3edde88fb3f3d
Paweł Brzozowski <pawel@brzoza.net>
2024-09-01 00:13:40 +0200

    State lifting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import { Form } from "./components/Form";
 import { LikesCounter } from "./components/LikesCounter";
 import { Plot } from "./components/Plot";
 import { Reviews } from "./components/Reviews";
 
+const backendReviews = [
+  {
+    id: nanoid(),
+    author: "Jan Kowalski",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, nesciunt!",
+  },
+  {
+    id: nanoid(),
+    author: "Amelia Sarnowska",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique excepturi maxime quis veritatis, dignissimos quia.",
+  },
+  {
+    id: nanoid(),
+    author: "Katarzyna Atemborska",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+  },
+];
+
 function App() {
+  const [reviews, setReviews] = useState(backendReviews);
   return (
     <>
       <h1>Wataha, sezon 1</h1>
       <h2>Rok produkcji: 2015</h2>
       <LikesCounter />
       <Plot />
-      <Reviews />
+      <Reviews state={reviews} />
+      <Form state={reviews} setter={setReviews} />
     </>
   );
 }
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,48 +1,12 @@
+/* eslint-disable react/prop-types */
+
 import { nanoid } from "nanoid";
-import { useState } from "react";
 import { Review } from "./Review";
 
-const backendReviews = [
-  {
-    id: nanoid(),
-    author: "Jan Kowalski",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, nesciunt!",
-  },
-  {
-    id: nanoid(),
-    author: "Amelia Sarnowska",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique excepturi maxime quis veritatis, dignissimos quia.",
-  },
-  {
-    id: nanoid(),
-    author: "Katarzyna Atemborska",
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
-  },
-];
-
-export const Reviews = () => {
-  const [reviews, setReviews] = useState(backendReviews);
-  const [inputValue, setInputValue] = useState("");
-  const [textareaValue, setTextareaValue] = useState("");
-
-  const reviewsElement = reviews.map((review) => (
+export const Reviews = ({ state }) => {
+  const reviewsElement = state.map((review) => (
     <Review author={review.author} text={review.text} key={nanoid()} />
   ));
-
-  const handleInput = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleTextaarea = (event) => {
-    setTextareaValue(event.target.value);
-  };
-
-  const handleReviewsForm = (event) => {
-    event.preventDefault();
-    const author = inputValue;
-    const text = textareaValue;
-    setReviews([...reviews, { id: nanoid(), author, text }]);
-  };
 
   return (
     <>
@@ -59,27 +23,6 @@ export const Reviews = () => {
       ) : (
         ""
       )} */}
-      <h2>Dodaj recenzję</h2>
-      <form onSubmit={handleReviewsForm}>
-        <div>
-          <div>
-            <label htmlFor="author">Autor:</label>
-          </div>
-          <input type="text" id="author" name="author" onChange={handleInput} />
-        </div>
-        <div>
-          <div>
-            <label htmlFor="text">Tekst:</label>
-          </div>
-          <textarea name="text" id="text" onChange={handleTextaarea}></textarea>
-        </div>
-        <button
-          type="submit"
-          disabled={inputValue === "" || textareaValue === ""}
-        >
-          Dodaj
-        </button>
-      </form>
     </>
   );
 };
