d726fd6121d249768499c3e5b961656c4da7d867
Paweł Brzozowski <pawel@brzoza.net>
2024-09-14 23:29:13 +0200

    Loader Spinner

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -1,36 +1,39 @@
-import { useState } from "react";
-import { Button } from "../Button/Button";
+import { useEffect, useState } from "react";
 import { List } from "../List/List";
 import css from "./Panel.module.css";
+import { Loader } from "../Loader/Loader";
 
 export function Panel() {
-  const [data, setData] = useState([
-    {
-      id: 1,
-      word: "kot",
-      translation: "cat",
-      category: "noun",
-    },
-    {
-      id: 2,
-      word: "pies",
-      translation: "dog",
-      category: "noun",
-    },
-    {
-      id: 3,
-      word: "skakać",
-      translation: "jump",
-      category: "verb",
-    },
-  ]);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/words")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        return data;
+      })
+      .then((data) => {
+        setData(data);
+        // NOTE: setTimeout to show the loader
+        // setIsLoading(false);
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <>
-      <Button>Załaduje dane</Button>
-      <section className={css.section}>
-        <List data={data}></List>
-      </section>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className={css.section}>
+          <List data={data}></List>
+        </section>
+      )}
     </>
   );
 }
