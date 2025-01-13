2ab3693621144698173faa496bff8a8e15f35e17
Paweł Brzozowski <pawel@brzoza.net>
2025-01-13 14:22:52 +0100

    Reset branch

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,23 +1,15 @@
 import css from "./List.module.css";
 import { Button } from "../Button/Button";
 import { nanoid } from "nanoid";
-import { RecipeContext } from "../../context/RecipeContext";
 
 export function List({ recipes, onSelectRecipe }) {
-  const selectedRecipeId = RecipeContext._currentValue.id;
-
   return (
     <nav className={css.nav}>
       <span className={css.title}>Lista przepisów:</span>
       <ul className={css.list}>
         {recipes.map(({ id, name }) => (
           <li key={nanoid()} className={css.item}>
-            <Button
-              onClick={() => onSelectRecipe(id)}
-              active={id === selectedRecipeId}
-            >
-              {name}
-            </Button>
+            <Button onClick={() => onSelectRecipe(id)}>{name}</Button>
           </li>
         ))}
       </ul>
