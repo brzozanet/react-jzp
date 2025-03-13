d9da041ab4f5bd28ad64345e52bb1b7538ca73cd
Paweł Brzozowski <pawel@brzoza.net>
2025-03-13 13:03:59 +0100

    Use createPortal for modal rendering in Section component

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
+import { createPortal } from "react-dom";
 import { Modal } from "../Modal/Modal";
 import "./Section.css";
 
 export function Section() {
   const [isModalShown, setIsModalShown] = useState(false);
+
+  const modal = createPortal(
+    <Modal setIsModalShown={setIsModalShown} />,
+    document.body
+  );
 
   const handleModalOpenClick = () => {
     console.log("click open");
@@ -17,7 +23,7 @@ export function Section() {
         <h3>Otwórz modal!</h3>
         <button onClick={() => handleModalOpenClick()}>Otwórz</button>
       </section>
-      {isModalShown && <Modal setIsModalShown={setIsModalShown} />}
+      {isModalShown && modal}
     </>
   );
 }
