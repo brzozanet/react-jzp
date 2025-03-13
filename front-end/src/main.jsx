18ce0fecd586bf6f1f350ed2c7df92ae2ba5515a
Paweł Brzozowski <pawel@brzoza.net>
2025-03-13 18:39:38 +0100

    Implement routing with React Router by creating a browser router and updating the main rendering logic

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "../src/App";
 
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    path: "/",
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
-    <h1>Hello React!</h1>
-  </>
+  <RouterProvider router={router} />
 );
