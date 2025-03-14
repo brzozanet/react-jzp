8fee77e7c738d563618f07f8b2cf77562add4b6b
Paweł Brzozowski <pawel@brzoza.net>
2025-03-14 08:01:26 +0100

    Add nanoid for unique keys in FoldersList and update folder links

--- front-end/src/components/FoldersList/FoldersList.jsx
+++ front-end/src/components/FoldersList/FoldersList.jsx
@@ -4,6 +4,7 @@ import { Folder } from "../Folder/Folder";
 import { Title } from "../Title/Title";
 import { TopBar } from "../TopBar/TopBar";
 import { AddNewButton } from "../AddNewButton/AddNewButton";
+import { nanoid } from "nanoid";
 
 const Folders = ({ children }) => (
   <div className={css["folders-column"]}>{children}</div>
@@ -39,8 +40,10 @@ export function FoldersList() {
         </TopBar>
         <Title>Foldery</Title>
         <UserCreatedFolders>
-          {folders.map((folder, idx) => (
-            <Folder key={idx}>{folder.name}</Folder>
+          {folders.map((folder) => (
+            <a key={nanoid()} href={`/notes/${folder.id}`}>
+              <Folder>{folder.name}</Folder>
+            </a>
           ))}
         </UserCreatedFolders>
         {/* <Folder icon="archive">Archiwum</Folder> */}
