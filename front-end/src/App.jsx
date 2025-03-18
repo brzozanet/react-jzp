9e619dbc8817c4c5995215d2f3ff5aba87ae5c6e
Paweł Brzozowski <pawel@brzoza.net>
2025-03-18 20:53:08 +0100

    Dodaj komponent Loader do App i obsługuj stan nawigacji

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { FoldersList } from "./components/FoldersList/FoldersList";
 import { Layout } from "./components/Layout/Layout";
+import { Loader } from "./components/Loader/Loader";
 
 export function App() {
+  const navigation = useNavigation();
+
   return (
-    <Layout>
-      <FoldersList />
-      <Outlet />
-    </Layout>
+    <>
+      <Layout>
+        {(navigation.state === "loading" ||
+          navigation.state === "submitting") && <Loader />}
+        <FoldersList />
+        <Outlet />
+      </Layout>
+    </>
   );
 }
