4ad3353230d17e43562e2bc09fcbbe5061ccb72e
Paweł Brzozowski <pawel@brzoza.net>
2025-07-22 01:50:28 +0200

    Add route "kontakt"

---
+++ 09-next/src/app/kontakt/page.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <>
+      <h2>Skontaktuj się ze mną</h2>
+      <p>tel. + 48 589 365 824</p>
+      <p>mail. pisz@naberdyczow.com</p>
+    </>
+  );
+}
