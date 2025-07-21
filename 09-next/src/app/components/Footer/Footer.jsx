683899cb41d6fa44542aebae1eb50329af54b488
Paweł Brzozowski <pawel@brzoza.net>
2025-07-22 01:22:34 +0200

    Usunięcie nieużywanych plików CSS oraz zmiany w strukturze komponentów w aplikacji Next.js. Zaktualizowano layout, dodano nagłówek i stopkę, a także zmieniono język na polski. Wprowadzono nową treść na stronie głównej.

---
+++ 09-next/src/app/components/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+import css from "./Footer.module.css";
+
+export const Footer = () => {
+  return <h2 className={css.footerTitle}>Stopka strony Hello Next!</h2>;
+};
---
+++ 09-next/src/app/components/Header/Header.jsx
@@ -0,0 +1,5 @@
+import css from "./Header.module.css";
+
+export const Header = () => {
+  return <h2 className={css.headerTitle}>Nagłówek strony Hello Next!</h2>;
+};
